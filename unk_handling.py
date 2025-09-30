1625855c2be79a11261e8d884e039f76df94bd76
hrishabd7 <hrishab1070@gmail.com>
2025-09-29 19:20:15 -0500

    Added replace_rare_with_unk_tokenized to unk_handling

--- unk_handling.py
+++ unk_handling.py
@@ -7,3 +7,12 @@ def replace_rare_with_unk_tokenized(sentences, rare_tokens, n):
         new_tokens = ["<unk>" if (token,) in rare_tokens else token for token in tokens]
         tokenized_reviews.append(new_tokens)
     return tokenized_reviews
+
+# Replace rare tokens in tokenized lists, not as joined strings
+def replace_rare_with_unk_tokenized(sentences, rare_tokens, n):
+    tokenized_reviews = []
+    for review in sentences:
+        tokens = tokenize_switch(review, n)
+        new_tokens = ["<unk>" if (token,) in rare_tokens else token for token in tokens]
+        tokenized_reviews.append(new_tokens)
+    return tokenized_reviews
